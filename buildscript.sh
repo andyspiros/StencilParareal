56a2e0c8d2fd2b46ba4a893990a9215484b82f21
Andrea Arteaga <andyspiros@gmail.com>
2014-01-12 19:13:38 +0100

    Added build script and organized jobs in the 'jobs' directory

---
+++ buildscript.sh
@@ -0,0 +1,99 @@
+#!/bin/bash -l
+
+function usage {
+    echo "bla bla bla"
+    exit 1
+}
+
+BASEDIR=$(pwd)
+
+TARGET=cpu
+BUILDDIR="${BASEDIR}/build"
+STELLADIR="stella"
+
+# Getting machine
+machine=$(hostname | sed -rn 's/^([a-z]+).*/\1/p')
+if [[ $machine == "ppopcode" ]]
+then
+    machine=opcode
+fi
+
+# Parsing options
+while getopts "t:b:s:" o
+do
+    case "${o}" in
+        t) # Target
+            if [[ ${OPTARG} == "gpu" ]]
+            then
+                TARGET=gpu
+            elif [[ ${OPTARG} == "cpu" ]]
+            then
+                TARGET=cpu
+            else
+                usage
+            fi
+            ;;
+
+        b) # Build directory
+            BUILDDIR=$(readlink --canonicalize "${OPTARG}")
+            ;;
+        
+        s) # Stencil library
+            STELLADIR=$(readlink --canonicalize "${OPTARG}")
+            ;;
+    esac
+done
+
+CMAKEARGS="
+    -DCMAKE_BUILD_TYPE=Release
+"
+
+# Programming environment
+loadedPrgEnv=$(module list 2>&1 | sed -rn 's|.*(PrgEnv-[a-z]+).*|\1|p')
+module switch $loadedPrgEnv PrgEnv-gnu
+export CC=cc
+export CXX=CC
+module load cmake
+
+# Boost
+if [[ $machine == "todi" ]]
+then
+    module load boost/1.51.0
+# TODO: Go on with other machines
+fi
+
+# CUDA
+if [[ $TARGET == "gpu" ]]
+then
+    CMAKEARGS+="
+        -DCUDA_BACKEND=ON
+        -DCUDA_NVCC_FLAGS=-I${MPICH_DIR}/include
+    "
+    module load cudatoolkit
+else
+    CMAKEARGS+="
+        -DCUDA_BACKEND=OFF
+    "
+fi
+
+# STELLA
+CMAKEARGS+="-DSTELLA_PATH=${STELLADIR}"
+
+
+# Call cmake
+mkdir -p $BUILDDIR
+cd $BUILDDIR
+cmake $BASEDIR $CMAKEARGS
+if [[ $? -gt 0 ]]
+then
+    echo "Error configuring. Aborting"
+    exit 2
+fi
+
+make -j8
+if [[ $? -gt 0 ]]
+then
+    echo "Error building. Aborting"
+    exit 3
+fi
+
